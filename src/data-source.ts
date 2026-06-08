b8e7fac3597e16bdbf2427c215d6140df31f8eac
danoninho27 <mateusmail224@gmail.com>
2026-06-08 15:54:07 -0300

    fix(migrations)

--- src/data-source.ts
+++ src/data-source.ts
@@ -4,6 +4,7 @@ import env from './env';
 import { GalleryItem } from './api/entities/GalleryItem';
 import { PecaItem } from './api/entities/PecaItem';
 import { CreateGalleryAndPecaTables1685620000000 } from './api/migrations/1685620000000-CreateGalleryAndPecaTables';
+import { AddShineToPecaItems1686000000000 } from './api/migrations/1686000000000-AddShineToPecaItems';
 import { AddImageToPecaItems1686000000001 } from './api/migrations/1686000000001-AddImageToPecaItems';
 
 export const AppDataSource = new DataSource({
@@ -14,7 +15,11 @@ export const AppDataSource = new DataSource({
   password: env.db_pass,
   database: env.db,
   entities: [GalleryItem, PecaItem],
-  migrations: [CreateGalleryAndPecaTables1685620000000, AddImageToPecaItems1686000000001],
+  migrations: [
+    CreateGalleryAndPecaTables1685620000000,
+    AddShineToPecaItems1686000000000,
+    AddImageToPecaItems1686000000001,
+  ],
   synchronize: false,
   logging: false,
 });
